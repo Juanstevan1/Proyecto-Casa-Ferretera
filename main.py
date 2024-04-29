947988de02bc69046247f71e39814e9823abed7e
Juanstevan1 <juanestebangarciagalvis@gmail.com>
2024-04-28 23:43:20 -0500

    Creacion carpeta templates

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 app = FastAPI()
+
+template = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
+def index(req: Request):
+    return template.TemplateResponse(
+        name = "index.html",
+        context = {"request": req}
+    )
+    
 async def root():
-    return {"message": "Hello World"}
+    return 
