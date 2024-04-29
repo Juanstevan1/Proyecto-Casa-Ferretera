8161af8e755521750fe6a68311e0743eff18ecb3
julimejia <davidmejiao43@gmail.com>
2024-04-29 04:18:27 -0500

    Requiements . txt update

--- main.py
+++ main.py
@@ -61,13 +61,6 @@ def upload_files(req: Request):
         context= {"request":req}
     )
 
-@app.get("/login")
-def login(req: Request):
-    return template.TemplateResponse(
-        name = "index2.html",
-        context = {"request": req}
-    )
-
 @app.get("/main")
 async def read_root(req: Request):
 
